7744dcb3a14d64ef14dca0f8dd115468478f7f3d
Christoph Friedrich <christoph@friedrich-whv.de>
2018-11-17 17:36:51 +0100

    Set up connection to MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var restify = require('restify');
 var server = restify.createServer();
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const dbUrl = 'mongodb://localhost:27017';
+const dbName = 'openeohub';
+
 server.get('/test', function(req, res, next) {
     res.send('test');
+    return next();
+});
+
+server.get('/crawl', function(req, res, next) {
+    MongoClient.connect(dbUrl, function(err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+      
+        const db = client.db(dbName);
+        const collection = db.collection('backends');
+    });
+    
     return next();
 });
 
