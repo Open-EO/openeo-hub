6cd77c4cea72c7fec6755e830710b9e03e808dc8
Christoph Friedrich <christoph@friedrich-whv.de>
2018-11-23 21:01:09 +0100

    Add UI for the already existing search parameters
    All search parameters that are already implemented (version, endpoints, collections, processes, process graph) can now be specified via the web app. The result is then requested from the API via Ajax and primitively displayed.
    
    Changed the Ajax library from `request` to `axios` - I don't know what made me install `request` in the first place... I like `axios` and we use it for the js-client too, so better use that library.
    
    The EndpointChooser could be generalised into a more versatile component, but for now it's good like it is.
    
    It would be better if the constant would be dynamically derived from the openEO API spec, but I didn't yet come up with a smart way to specify the dependency.

--- src/Hub.vue
+++ src/Hub.vue
@@ -1,21 +1,84 @@
 <template>
 	<div id="container">
 		<h1>openEO hub</h1>
+
+		<h2>Search</h2>
+
+		<h3>Version</h3>
+		<div>
+			<input type="radio" value="any"   v-model="selectedVersion" id="vany"><label for="vany">any</label>
+			<input type="radio" value="0.3.0" v-model="selectedVersion" id="v030"><label for="v030">0.3.0</label>
+			<input type="radio" value="0.3.1" v-model="selectedVersion" id="v031"><label for="v031">0.3.1</label>
+		</div>
+
+		<h3>Endpoints</h3>
+		<EndpointChooser :endpoints="allEndpoints" :calledOnChange="setSelectedEndpoints"></EndpointChooser>
+
+		<h3>Collections</h3>
+		<textarea v-model="specifiedCollections" placeholder="Specify collection identifiers, each on a new line"></textarea>
+
+		<h3>Processes</h3>
+		<textarea v-model="specifiedProcesses" placeholder="Specify process identifiers, each on a new line"></textarea>
+
+		<h3>Process Graph</h3>
+		<textarea v-model="specifiedProcessGraph" placeholder="Paste an openEO process graph"></textarea>
+
+		<h3>Actions</h3>
+		<button @click="queryBackends()">Submit</button>
+
+		<h2>Result</h2>
+		<em v-if="matchedBackends.length == 0">empty</em>
+		<output>
+			<ul>
+				<li v-for="backend in matchedBackends" :key="backend">{{backend}}</li>
+			</ul>
+		</output>
 	</div>
 </template>
 
 <script>
 import Vue from 'vue';
+import axios from 'axios';
+import { OPENEO_V0_3_1_ENDPOINTS } from './const.js'
+import EndpointChooser from './components/EndpointChooser.vue';
 
 export default {
 	name: 'openeo-hub',
 	components: {
+		EndpointChooser
 	},
 	data() {
 		return {
+			selectedVersion: 'any',
+			// sort alphabetically by endpoint path (i.e. delete HTTP method (always uppercased) for sorting)
+			allEndpoints: OPENEO_V0_3_1_ENDPOINTS.sort((e1, e2) => e1.replace(/[A-Z]/g, '') > e2.replace(/[A-Z]/g, '')),
+			selectedEndpoints: [],
+			specifiedCollections: '',
+			specifiedProcesses: '',
+			specifiedProcessGraph: '',
+			matchedBackends: []
 		};
 	},
 	methods: {
+		setSelectedEndpoints(input) {
+			this.selectedEndpoints = input;
+		},
+		queryBackends() {
+			const params = {
+				version:             (this.selectedVersion == 'any' ? undefined : this.selectedVersion),
+				endpoints:      (this.selectedEndpoints.length == 0 ? undefined : this.selectedEndpoints),
+				collections: (this.specifiedCollections.length == 0 ? undefined : this.specifiedCollections.split("\n")),
+				processes:     (this.specifiedProcesses.length == 0 ? undefined : this.specifiedProcesses.split("\n")),
+				processGraph:     (this.specifiedProcessGraph == '' ? undefined : JSON.parse(this.specifiedProcessGraph))
+			}
+			axios.post('/backends/search', params)
+				.then(response => {
+					this.matchedBackends = response.data;
+				})
+				.error(error => {
+					alert(error);
+				});
+		}
 	}
 }
 </script>
@@ -26,10 +89,13 @@ html, body, #app, #container {
 }
 body {
 	margin: 0;
-	overflow: hidden;
 	font-family: sans-serif;
 	font-size: 90%;
 }
+h1, h2, h3 {
+	margin-bottom: 0;
+}
+
 a {
 	color: #2F649A;
 	text-decoration: none;
@@ -38,6 +104,7 @@ a {
 a:hover {
 	color: black;
 }
+
 button {
 	margin: 1px;
 }
@@ -46,5 +113,17 @@ ul, ol {
 	margin-top: 0;
 	padding-bottom: 0;
 	padding-top: 0;
+}
+
+#endpointchooser {
+	/*column-count: 4;*/
+	height: 100px;
+	width: 400px;
+	overflow-y: scroll;
+}
+textarea {
+	width: 50vw;
+	min-width: 300px;
+	height: 100px;
 }
 </style>
---
+++ src/components/EndpointChooser.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="endpointchooser">
+		<ul>
+			<li v-for="(endpoint, index) in endpoints" :key="endpoint">
+				<input type="checkbox" :value="endpoint" v-model="selectedEndpoints" @change="returnState()" :id="'endpoint'+index">
+				<label :for="'endpoint'+index">{{endpoint}}</label>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'EndpointChooser',
+	props: ['endpoints', 'calledOnChange'],
+	data() {
+		return {
+			selectedEndpoints: []
+		}
+	},
+	methods: {
+		returnState() {
+			this.calledOnChange(this.selectedEndpoints);
+		}
+	}
+}
+</script>
+
+<style scoped>
+	ul {
+		padding: 0;
+	}
+	li {
+		list-style: none;
+	}
+</style>
