Open-EO/openeo-hub

commit 3bd241c19edaa3abbe0ca720e12b79d3f0521ff0

Refactor: Backend as own component, rename Collection
The backend rendering should be encapsulated in its own component.
The ProcessPanel in processes-docgen was renamed to Process, so by analogy the CollectionPanel should be renamed to Collection too.

src/components/BackendResults.vue
@@ -3,70 +3,21 @@
         <h2>Results from backend search {{(matchedBackends ? '('+matchedBackends.length+')' : '')}}</h2>
         <em v-if="matchedBackends == null" class="emptyNotice">{{initialInstructionText}}</em>
         <em v-else-if="matchedBackends.length == 0" class="emptyNotice">No search results.</em>
-        <output>
-            <ol>
-                <li v-for="backend in matchedBackends" :key="backend.backend">
-                    <h3>{{backend.backend}}</h3>
-                    <dl>
-                        <dt><h4>Version</h4></dt>
-                        <dd>{{backend.version}}</dd>
-
-                        <dt v-if="backend.endpoints"><h4>Matched endpoints ({{backend.endpoints.length}})</h4></dt>
-                        <dd v-if="backend.endpoints">
-                            <ul>
-                                <li v-for="endpoint in backend.endpoints" :key="endpoint">{{endpoint}}</li>
-                            </ul>
-                        </dd>
-
-                        <dt v-if="backend.collections"><h4>Matched collections ({{backend.collections.length}})</h4></dt>
-                        <dd v-if="backend.collections">
-                            <CollectionPanel v-for="collection in backend.collections" :key="collection.name" :collection="collection" initiallyCollapsed="true"></CollectionPanel>
-                        </dd>
-                        
-                        <dt v-if="backend.processes"><h4>Matched processes ({{backend.processes.length}})</h4></dt>
-                        <dd v-if="backend.processes">
-                            <Process v-for="process in backend.processes" :key="process.name || process.id" :process="convertProcessToLatestSpec(process)" :baseConfig="{processesInitiallyCollapsed:true}"></Process>
-                        </dd>
-
-                        <dt v-if="backend.outputFormats"><h4>Matched output formats ({{Object.keys(backend.outputFormats).length}})</h4></dt>
-                        <dd v-if="backend.outputFormats">
-                            <ul>
-                                <li v-for="of in Object.keys(backend.outputFormats)" :key="of">{{of}}</li>
-                            </ul>
-                        </dd>
-
-                        <dt v-if="backend.serviceTypes"><h4>Matched service types ({{Object.keys(backend.serviceTypes).length}})</h4></dt>
-                        <dd v-if="backend.serviceTypes">
-                            <ul>
-                                <li v-for="st in Object.keys(backend.serviceTypes)" :key="st">{{st}}</li>
-                            </ul>
-                        </dd>
-                    </dl>
-                    <div class="retrieved">
-                        <em>This data was retrieved from the backend server at {{backend.retrieved}}.</em>
-                    </div>
-                </li>
-            </ol>
-        </output>
+        <ol>
+            <li v-for="backend in matchedBackends" :key="backend.backend" class="backendPanelParent">
+                <Backend :backend="backend" :initiallyCollapsed="true"></Backend>
+            </li>
+        </ol>
     </div>
 </template>
 
 <script>
-import { Process, Utils as DocGenUtils } from '@openeo/processes-docgen';
-import CollectionPanel from './CollectionPanel.vue';
+import Backend from './Backend.vue';
 
 export default {
     name: 'BackendResults',
-    components: {
-        CollectionPanel,
-        Process
-    },
-    props: ['matchedBackends', 'initialInstructionText'],
-    methods: {
-        convertProcessToLatestSpec(proc) {
-            return DocGenUtils.convertProcessToLatestSpec(proc);
-        }
-    }
+    components: { Backend },
+    props: ['matchedBackends', 'initialInstructionText']
 }
 </script>
 

src/components/Collection.vue
@@ -83,7 +83,7 @@ import DataRetrievedNotice from './DataRetrievedNotice.vue';
 import FormattedTimestamp from './FormattedTimestamp.vue';
 
 export default {
-	name: 'CollectionPanel',
+	name: 'Collection',
 	props: ['collection', 'initiallyCollapsed'],
 	components: {
 		Description,

src/components/CollectionResults.vue
@@ -5,18 +5,18 @@
         <em v-else-if="matchedCollections.length == 0" class="emptyNotice">No search results.</em>
         <ol>
             <li v-for="collection in matchedCollections" :key="collection.backend+'/'+collection.name" class="collectionPanelParent">
-                <CollectionPanel :collection="collection" :initiallyCollapsed="true"></CollectionPanel>
+                <Collection :collection="collection" :initiallyCollapsed="true"></Collection>
             </li>
         </ol>
     </div>
 </template>
 
 <script>
-import CollectionPanel from './CollectionPanel.vue';
+import Collection from './Collection.vue';
 
 export default {
     name: 'CollectionResults',
-    components: { CollectionPanel },
+    components: { Collection },
     props: ['matchedCollections', 'initialInstructionText']
 }
 </script>

src/components/Backend.vue
@@ -1,0 +1,72 @@
+<template>
+	<div class="backendPanel">
+        <h3>{{backend.backend}}</h3>
+        <dl>
+            <dt><h4>Version</h4></dt>
+            <dd>{{backend.version}}</dd>
+
+            <dt v-if="backend.endpoints"><h4>Matched endpoints ({{backend.endpoints.length}})</h4></dt>
+            <dd v-if="backend.endpoints">
+                <ul>
+                    <li v-for="endpoint in backend.endpoints" :key="endpoint">{{endpoint}}</li>
+                </ul>
+            </dd>
+
+            <dt v-if="backend.collections"><h4>Matched collections ({{backend.collections.length}})</h4></dt>
+            <dd v-if="backend.collections">
+                <Collection v-for="collection in backend.collections" :key="collection.name" :collection="collection" initiallyCollapsed="true"></Collection>
+            </dd>
+            
+            <dt v-if="backend.processes"><h4>Matched processes ({{backend.processes.length}})</h4></dt>
+            <dd v-if="backend.processes">
+                <Process v-for="process in backend.processes" :key="process.name || process.id" :process="convertProcessToLatestSpec(process)" :baseConfig="{processesInitiallyCollapsed:true}"></Process>
+            </dd>
+
+            <dt v-if="backend.outputFormats"><h4>Matched output formats ({{Object.keys(backend.outputFormats).length}})</h4></dt>
+            <dd v-if="backend.outputFormats">
+                <ul>
+                    <li v-for="of in Object.keys(backend.outputFormats)" :key="of">{{of}}</li>
+                </ul>
+            </dd>
+
+            <dt v-if="backend.serviceTypes"><h4>Matched service types ({{Object.keys(backend.serviceTypes).length}})</h4></dt>
+            <dd v-if="backend.serviceTypes">
+                <ul>
+                    <li v-for="st in Object.keys(backend.serviceTypes)" :key="st">{{st}}</li>
+                </ul>
+            </dd>
+        </dl>
+        <div class="retrieved">
+            <DataRetrievedNotice :timestamp="backend.retrieved"></DataRetrievedNotice>
+        </div>
+    </div>
+</template>
+
+<script>
+import DataRetrievedNotice from './DataRetrievedNotice.vue';
+import { Process, Utils as DocGenUtils } from '@openeo/processes-docgen';
+import Collection from './Collection.vue';
+
+export default {
+	name: 'Backend',
+	props: ['backend', 'initiallyCollapsed'],
+	components: {
+        DataRetrievedNotice,
+        Collection,
+        Process
+	},
+	data() {
+		return {
+			collapsed: this.initiallyCollapsed || false
+		};
+    },
+    methods: {
+        convertProcessToLatestSpec(proc) {
+            return DocGenUtils.convertProcessToLatestSpec(proc);
+        }
+    }
+}
+</script>
+
+<style>
+</style>

commit 06c4f98f2638d7a6ceb9b0e622b2c8cb2426aa7b

Fix CSS that got broken during refactoring in 3bd241c
- renaming of `CollectionPanel` to `Collection` should be consistent, i.e. also in the CSS class names
- removed the `output` tag (because it's not used consistently) but forgot that it was still part of a CSS selector

src/Hub.vue
@@ -369,7 +369,7 @@ main nav li.active {
 #results .emptyNotice {
 	margin-left: 20px;
 }
-#results output > ol > li { /* with "direct child" selectors so that it doesn't affect `li`s of other lists further down */
+ol.searchresults > li { /* with "direct child" selectors so that it doesn't affect `li`s of other lists further down */
 	margin-bottom: 20px;
 }
 #results dd {
@@ -383,13 +383,13 @@ main nav li.active {
 }
 #results .process h2,
 #results .process h3,
-#results .collectionPanel h2,
-#results .collectionPanel h3 {
+#results .collection h2,
+#results .collection h3 {
 	font-size: 100%;
 	margin-top: 10px;
 }
-#results .collectionPanel + .collectionPanel,
-#results .collectionPanelParent + .collectionPanelParent,
+#results .collection + .collection,
+#results .collectionParent + .collectionParent,
 #results .process + .process,
 #results .processParent + .processParent {
 	/* margin between neighbouring panels */
@@ -397,7 +397,7 @@ main nav li.active {
 }
 
 /* Panels */
-div:not(.collectionPanel):not(.process) > h2 { /* all normal `h2`s, but not the ones that are part of the `Process` */
+div:not(.collection):not(.process) > h2 { /* all normal `h2`s, but not the ones that are part of the `Process` */
 	position: sticky;
 	top: 0;
 	z-index: 2000; /* would lie under Leaflet map (500), attribution layer (800) or corner boxes (1000) if less */
@@ -407,7 +407,7 @@ div:not(.collectionPanel):not(.process) > h2 { /* all normal `h2`s, but not the 
 	padding-bottom: 10px;
 	border-bottom: 1px dotted #cecbc8;
 }
-.collectionPanel,
+.collection,
 .process {
 	/* set `position` because... */
 	position: relative;

src/components/BackendResults.vue
@@ -3,7 +3,7 @@
         <h2>Results from backend search {{(matchedBackends ? '('+matchedBackends.length+')' : '')}}</h2>
         <em v-if="matchedBackends == null" class="emptyNotice">{{initialInstructionText}}</em>
         <em v-else-if="matchedBackends.length == 0" class="emptyNotice">No search results.</em>
-        <ol>
+        <ol class="searchresults">
             <li v-for="backend in matchedBackends" :key="backend.backend" class="backendPanelParent">
                 <Backend :backend="backend" :initiallyCollapsed="true"></Backend>
             </li>

src/components/Collection.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="collectionPanel">
+	<div class="collection">
 		<a class="anchor" :name="collection.name"></a><!-- ToDo: Replace name with id -->
 		<h2>{{collection.name}}</h2>
 

src/components/CollectionResults.vue
@@ -4,7 +4,7 @@
         <em v-if="matchedCollections == null" class="emptyNotice">{{initialInstructionText}}</em>
         <em v-else-if="matchedCollections.length == 0" class="emptyNotice">No search results.</em>
         <ol>
-            <li v-for="collection in matchedCollections" :key="collection.backend+'/'+collection.name" class="collectionPanelParent">
+            <li v-for="collection in matchedCollections" :key="collection.backend+'/'+collection.name" class="collectionParent">
                 <Collection :collection="collection" :initiallyCollapsed="true"></Collection>
             </li>
         </ol>