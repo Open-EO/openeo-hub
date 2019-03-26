2d6ae764c1bb8dee11ad2ad337314a9af0dee664
Christoph Friedrich <christoph@friedrich-whv.de>
2019-03-26 15:59:27 +0100

    Show warning sign if data is old or crawling failed (addresses #13)
    Like @m-mohr suggested: "Put a warning sign after the title of the back-end and put the description [...] in a tooltip". The text is a bit more generic though: It doesn't show the crawl time, but just the statement that the data is older than the configured threshold. Likewise, it just states that the backend was recently unavailable for crawling, not how often this was the case or when the last successful crawl was.
    
    This commit introduces a new config option `flagWhenOlderThanXHours`.

--- src/components/BackendName.vue
+++ src/components/BackendName.vue
@@ -6,13 +6,36 @@
         <template v-if="data.version">
             | v{{data.version}}
         </template>
+        <span v-if="needsWarningSign" :title="warningText">️⚠</span>
     </span>
 </template>
 
 <script>
+import {default as config} from './../../config.json';
+
 export default {
     name: 'BackendName',
-    props: ['data']
+    props: ['data'],
+    computed: {
+        recentlyUnavailable() {
+            return this.data.unsuccessfulCrawls >= config.unsuccessfulCrawls.flagAfter;
+        },
+        recentlyUnavailableText() {
+            return this.recentlyUnavailable ? 'This backend was recently unavailable for crawling.' : '';
+        },
+        oldData() {
+            return (new Date() - new Date(this.data.retrieved)) >= config.flagWhenOlderThanXHours * 60 * 60 * 1000;
+        },
+        oldDataText() {
+            return this.oldData ? 'This data was cached more than ' + config.flagWhenOlderThanXHours + ' hours ago.' : '';
+        },
+        needsWarningSign() {
+            return this.recentlyUnavailable || this.oldData;
+        },
+        warningText() {
+            return [this.recentlyUnavailableText, this.oldDataText].join(' ');
+        }
+    }
 }
 </script>
 
