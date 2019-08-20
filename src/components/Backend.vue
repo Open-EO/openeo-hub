2393307b0cf4ffbb1039048cf274f881d6d72095
Christoph Friedrich <christoph@friedrich-whv.de>
2019-08-20 15:53:24 +0200

    Use `api_version` property instead of `version`
    (follow-up on 518f4a8)
    -> less ambiguity
    -> conform to API 0.4.x
    -> if a value is given in `version` property but not in `api_version` (e.g. spec-compliant v0.3.x backend), it is copied to `api_version` (line 126)

--- src/components/Backend.vue
+++ src/components/Backend.vue
@@ -21,42 +21,42 @@
                 <h4>{{collapsed.functionalities ? '▶' : '▼'}} {{isSearchResult ? 'Matched' : 'Supported'}} functionalities {{supportedFunctionalitiesCount}}</h4>
             </dt>
             <dd v-show="backend.endpoints && !collapsed.functionalities">
-                <SupportedFeatures :endpoints="preparedBackend.endpoints" :version="preparedBackend.version" ref="supportedFeaturesComponent"></SupportedFeatures>
+                <SupportedFeatures :endpoints="preparedBackend.endpoints" :version="preparedBackend.api_version" ref="supportedFeaturesComponent"></SupportedFeatures>
             </dd>
 
             <dt v-if="backend.collections" @click="toggleCollections">
                 <h4>{{collapsed.collections ? '▶' : '▼'}} {{isSearchResult ? 'Matched' : 'All'}} collections ({{backend.collections.length}})</h4>
             </dt>
             <dd v-if="backend.collections && !collapsed.collections">
-                <CollectionWrapper v-for="collection in preparedBackend.collections" :key="collection.name" :collectionData="collection" :version="preparedBackend.version" :initiallyCollapsed="true"></CollectionWrapper>
+                <CollectionWrapper v-for="collection in preparedBackend.collections" :key="collection.name" :collectionData="collection" :version="preparedBackend.api_version" :initiallyCollapsed="true"></CollectionWrapper>
             </dd>
             
             <dt v-if="backend.processes" @click="toggleProcesses">
                 <h4>{{collapsed.processes ? '▶' : '▼'}} {{isSearchResult ? 'Matched' : 'All'}} processes ({{backend.processes.length}})</h4>
             </dt>
             <dd v-if="preparedBackend.processes && !collapsed.processes">
-                <ProcessWrapper v-for="process in backend.processes" :key="process.name || process.id" :processData="process" :version="preparedBackend.version" :initiallyCollapsed="true" :provideDownload="false"></ProcessWrapper>
+                <ProcessWrapper v-for="process in backend.processes" :key="process.name || process.id" :processData="process" :version="preparedBackend.api_version" :initiallyCollapsed="true" :provideDownload="false"></ProcessWrapper>
             </dd>
 
             <dt v-if="backend.outputFormats" @click="collapsed.outputFormats = !collapsed.outputFormats">
                 <h4>{{collapsed.outputFormats ? '▶' : '▼'}} {{isSearchResult ? 'Matched' : 'All'}} output formats ({{supportedOutputFormatsCount}})</h4>
             </dt>
             <dd v-if="backend.outputFormats" v-show="!collapsed.outputFormats"> <!-- v-if to prevent errors when outputFormats is not present. If it is present: v-show to always render -> allow retrieval of item count (-> heading) from SupportedFileFormats component -->
-                <SupportedFileFormats :formats="preparedBackend.outputFormats" :version="preparedBackend.version" ref="supportedFileFormatsComponent"></SupportedFileFormats>
+                <SupportedFileFormats :formats="preparedBackend.outputFormats" :version="preparedBackend.api_version" ref="supportedFileFormatsComponent"></SupportedFileFormats>
             </dd>
 
             <dt v-if="backend.serviceTypes" @click="collapsed.serviceTypes = !collapsed.serviceTypes">
                 <h4>{{collapsed.serviceTypes ? '▶' : '▼'}} {{isSearchResult ? 'Matched' : 'All'}} service types ({{Object.keys(backend.serviceTypes).length}})</h4>
             </dt>
             <dd v-if="backend.serviceTypes && !collapsed.serviceTypes">
-                <SupportedServiceTypes :services="preparedBackend.serviceTypes" :version="preparedBackend.version"></SupportedServiceTypes>
+                <SupportedServiceTypes :services="preparedBackend.serviceTypes" :version="preparedBackend.api_version"></SupportedServiceTypes>
             </dd>
 
             <dt v-if="backend.billing" @click="collapsed.billing = !collapsed.billing">
                 <h4>{{collapsed.billing ? '▶' : '▼'}} Billing information</h4>
             </dt>
             <dd v-if="backend.billing && !collapsed.billing" class="billing">
-                <BillingPlans :billing="backend.billing" :version="preparedBackend.version"></BillingPlans>
+                <BillingPlans :billing="backend.billing" :version="preparedBackend.api_version"></BillingPlans>
             </dd>
         </dl>
 
@@ -119,6 +119,7 @@ export default {
         }
 
         this.preparedBackend = original;
+        this.preparedBackend.api_version = this.preparedBackend.api_version || this.preparedBackend.version;
     },
 
     computed: {
