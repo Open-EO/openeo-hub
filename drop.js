4741119fdb615d61031b58fb9754d60d9a8d8630
Christoph Friedrich <christoph@friedrich-whv.de>
2020-03-05 15:34:17 +0100

    Catch all possible Promise rejections in drop script

--- drop.js
+++ drop.js
@@ -3,8 +3,14 @@ const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
 const mongo = new MongoClient(config.dbUrl, { useNewUrlParser: true } );
 console.log('Connecting to database server...');
-mongo.connect((err, client) => {
-    assert.equal(null, err);
+mongo.connect((error, client) => {
+    if(error != null) {
+        console.log(`An error occurred while connecting to the database server (${error.name}: ${error.message})`);
+        console.log('');
+        console.log('DROPPING ABORTED!')
+        return 1;  // abort with non-zero exit code
+    }
+    
     console.log('Connected.');
     const db = client.db(config.dbName);
 
@@ -15,7 +21,7 @@ mongo.connect((err, client) => {
         console.log('You must start the script with --yesimsure parameter to take effect');
         console.log('I.e. you must call `node drop.js --yesimsure` or `npm run drop -- --yesimsure`)');
         console.log('Exiting without doing anything.');
-        mongo.close();
+        mongo.close().catch(console.log);
     } else {
         var collectionsToDrop = ['raw', 'backends', 'collections', 'processes'];
         if(dropEverything) {
@@ -26,15 +32,15 @@ mongo.connect((err, client) => {
             if(colls.length == 0) {
                 console.log('No collections exist that could be dropped.')
                 console.log('Exiting without doing anything.');
-                mongo.close();
+                mongo.close().catch(console.log);
             } else {
                 Promise
-                .all(colls.map(c => db.dropCollection(c.name)))
+                .all(colls.map(c => db.dropCollection(c.name).catch(console.log)))
                 .then(() => console.log('Successfully dropped all collections' + (dropEverything ? '' : ' (except user-generated content)') + '.'))
                 .catch(e => console.log(e))
-                .then(() => mongo.close());  // always executed
+                .then(() => mongo.close().catch(console.log));  // always executed
             }
         })
-        .catch(e => {console.log(e); mongo.close(); });
+        .catch(e => {console.log(e); mongo.close().catch(console.log); });
     }
 });
