60d222fcf18a3b65e8d089a946f55625d096c7cb
Christoph Friedrich <christoph@friedrich-whv.de>
2021-07-21 14:57:06 +0200

    Add "how to add a backend" to About page (closes #27)

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -4,5 +4,6 @@
 		<p>The data is crawled from the services and then cached, so may lag behind reality.</p>
 		<p>For more information on openEO, visit the project's homepage: <a href="https://openeo.org/">https://openeo.org/</a></p>
 		<p>The source code of this website is available <a href="https://github.com/Open-EO/openeo-hub">on GitHub</a>.</p>
+		<p>If you want to add a service to this index, please open a pull request on GitHub that edits <a href="https://github.com/Open-EO/openeo-hub/blob/master/config.json">the config.json file</a> accordingly. We will then review and merge it and after a quick redeployment the crawler will index your service too. Any newcomers are very welcome!</p>
 	</section>
 </template>
