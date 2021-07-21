44b393b97a08f2e9425b0d41636e15e5e710cff6
Christoph Friedrich <christoph@friedrich-whv.de>
2021-07-21 15:33:24 +0200

    Improve wording and design of About page

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,9 +1,15 @@
 <template>
     <section id="about">
-		<p><strong>openEO hub</strong> is a tool to discover services that support the openEO API. It is also an exchange platform to share process graphs among the openEO community.</p>
-		<p>The data is crawled from the services and then cached, so may lag behind reality.</p>
-		<p>For more information on openEO, visit the project's homepage: <a href="https://openeo.org/">https://openeo.org/</a></p>
+		<p><strong>openEO Hub</strong> is a tool to discover Earth Observation cloud processing services that support the <a href="https://github.com/Open-EO/openeo-api">openEO API</a>. <!-- It is also an exchange platform to share process graphs among the openEO community. --> For more information on openEO, visit the project's homepage: <a href="https://openeo.org/">https://openeo.org/</a></p>
+		<p>Please note: The data is crawled from the services and then cached, so may lag behind reality.</p>
 		<p>The source code of this website is available <a href="https://github.com/Open-EO/openeo-hub">on GitHub</a>.</p>
 		<p>If you want to add a service to this index, please open a pull request on GitHub that edits <a href="https://github.com/Open-EO/openeo-hub/blob/master/config.json">the config.json file</a> accordingly. We will then review and merge it and after a quick redeployment the crawler will index your service too. Any newcomers are very welcome!</p>
 	</section>
 </template>
+
+<style scoped>
+	section {
+		margin: 50px auto;
+		max-width: 800px;
+	}
+</style>
